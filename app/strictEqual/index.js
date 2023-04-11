1931dcf72d5fa08e9d852de2bf3bd62c6a01d5f0
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-04-11 23:17:23 +0200

    Add function strictEqual and export it

---
+++ app/strictEqual/index.js
@@ -0,0 +1,17 @@
+const strictEqual = (valueA, valueB) => {
+  if (Number.isNaN(valueA) && Number.isNaN(valueB)) {
+    return false;
+  }
+
+  if (!Number.isNaN(valueA) && !valueA && !Number.isNaN(valueB) && !valueB) {
+    return true;
+  }
+
+  if (Object.is(valueA, valueB)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export { strictEqual };
